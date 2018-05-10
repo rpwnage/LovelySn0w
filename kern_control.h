c077513a11535b13aa546c5ff058758e1dedbed6
iSn0we <lucapasc@lucas-MBP.fritz.box>
2018-05-10 17:05:27 +0200

    first commit

---
+++ kern_control.h
@@ -0,0 +1,13 @@
+//
+//  kern_control.h
+//  LovelySn0w
+//
+//  Created by luca pasc on 10.05.18.
+//  Copyright © 2018 iSn0w. All rights reserved.
+//
+
+#ifndef kern_control_h
+#define kern_control_h
+
+
+#endif /* kern_control_h */
